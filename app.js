7f2a81600218a2d14d9c8d7bbc36a5fbb124c2fa
Mike Ziehfreund <mikeziehfreund@gmail.com>
2018-04-04 12:08:49 -0400

    on drill 3

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-let loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-        return (this.water / this.flour) * 100
-    }
-};
+'use strict'
 
-// console.log(loaf.hydration());
+
+// let loaf = {
+//     flour: 300,
+//     water: 210,
+//     hydration: function() {
+//         return (this.water / this.flour) * 100
+//     }
+// };
+
+// // console.log(loaf.hydration());
+
+// --- DRILL 2
+
+// let object = {
+//     foo: 25,
+//     bar: 7,
+//     fum: 'mike',
+//     quux: 'sayed',
+//     spam: 1,
+// };
+//     for (let key in object) {
+//         console.log(object[key]);
+//     };   
+
+// DRILL 3
+
+let food = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+}
+
+console.log(food.meals[3]);
